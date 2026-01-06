d6923e7b98167b09748048e1ed33560abd9e57a8
Rommel <rommel.parungao@bodwell.edu>
2026-01-06 13:04:15 -0800

    azure login update

---
+++ app/Providers/EventServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\Facades\Event;
+use Illuminate\Support\ServiceProvider;
+use SocialiteProviders\Azure\AzureExtendSocialite;
+use SocialiteProviders\Manager\SocialiteWasCalled;
+
+class EventServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        Event::listen(SocialiteWasCalled::class, [AzureExtendSocialite::class, 'handle']);
+    }
+}
